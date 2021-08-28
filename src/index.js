b1522144cf70a329db33f89b3975d964dc83603d
FeuerImBlut <golubovich.as@gmail.com>
2021-08-28 14:27:37 +0300

    update: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+	module.exports = function check(str, bracketsConfig) {
+		if (str.length %2 > 0) {
+			return false;
+		}
+		
+		for (let i = str.length - 1; i >= 0 ; i--)
+		{
+			for (let j = 0; j < bracketsConfig.length; j++)
+			{
+				if (str[i] === bracketsConfig[j][0]) {					
+					str = str.replace(bracketsConfig[j].join(""), "")
+				}
+			}
+
+		}
+		if (str.length === 0) {
+			return true;
+		}
+		else {
+			return false;
+			}
 }
